f74407905e7df4ebc6cd593200ae60dcbd2321df
Francis Robichaud <frobichaud@petalmd.com>
2016-04-04 14:49:26 -0400

    iOS demo project with ActionCable

---
+++ ios/model3/model3/SalesViewController.swift
@@ -0,0 +1,93 @@
+//
+//  TableViewController.swift
+//  model3
+//
+//  Created by Francis Robichaud on 2016-04-04.
+//  Copyright © 2016 PetalMD. All rights reserved.
+//
+import UIKit
+import Foundation
+import ActionCableClient
+import Alamofire
+
+class SalesViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let WS_HOST = "127.0.0.1"
+    let WS_PORT = 3000
+    
+    private var client:ActionCableClient?
+    
+    struct Sale {
+        var name  = ""
+        var amount = 0
+    }
+    
+    @IBOutlet weak var _total: UILabel!
+    @IBOutlet weak var _tableView: UITableView!
+    
+    private var _data = [Sale]()
+    private var _currentTotal = 0
+    
+    override func viewDidLoad() {
+        getSales()
+        
+        client = ActionCableClient(URL: NSURL(string: "ws://\(WS_HOST):\(WS_PORT)/sales_cable")!)
+        client!.origin = WS_HOST
+        client!.connect()
+        
+        client!.create("SalesChannel")
+        client!.onChannelReceive = onReceive
+    }
+    
+    // MARK: Websocket
+    func onReceive(chan:Channel, obj:AnyObject?, error:ErrorType?) {
+        let response = obj as! NSDictionary
+        let name = response["name"] as! String
+        let amount = response["amount"] as! Int
+        
+        _data.insert(Sale(name: name, amount: amount), atIndex: 0)
+        _tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Top)
+        _currentTotal += amount
+        updateTotal()
+    }
+    
+    private func updateTotal() {
+        self._total!.text = "$\(self._currentTotal)"
+    }
+    
+    // MARK: REST API
+    private func getSales() {
+        Alamofire.request(.GET, "http://\(WS_HOST):\(WS_PORT)/sales/index.json")
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    let response = JSON as! NSDictionary
+                    self._currentTotal = response["total"] as! Int
+                    self.updateTotal()
+                    self._data = (response["sales"] as! NSArray).map { sale in
+                        let name = String(sale["name"]!)
+                        return Sale(name: name, amount: 2)
+                    }
+                    self._tableView.reloadData()
+                }
+        }
+    }
+    
+    // MARK: Table View
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return _data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("random")
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "random")
+        }
+        
+        cell!.textLabel!.text = _data[indexPath.row].name
+        return cell!
+    }
+}
--- rails/app/controllers/sales_controller.rb
+++ rails/app/controllers/sales_controller.rb
@@ -2,5 +2,15 @@ class SalesController < ApplicationController
   def index
     @sales = Sale.last(10).reverse
     @total = Sale.sum :amount
+
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: {
+            total: @total,
+            sales: @sales.collect {|sale| {name: sale.name, amount: sale.amount}}
+        }
+      }
+    end
   end
 end
